148ba55d6190fc56371df7e59ce632a327ca2b44
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-02-11 13:52:08 +0100

    UI working with backend, no pages.

--- ChalkBoardChat.UI/Program.cs
+++ ChalkBoardChat.UI/Program.cs
@@ -1,7 +1,27 @@
+
+
+
+
+
+using ChalkBoardChat.BLL.Services;
+using ChalkBoardChat.DAL.Contexts;
+using ChalkBoardChat.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+
+builder.Services.AddScoped<IMessageService, MessageService>();
+
+
 
 var app = builder.Build();
 
